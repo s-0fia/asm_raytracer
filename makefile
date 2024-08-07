ba7fc7a5dfa9109b98cb57906a4c4509a0a93b4b
Elle Fleming <ellefleming455@gmail.com>
2024-08-08 10:49:53 +1200

    Initialised project and started main + Basic proj init + Make main get width+height from cli args + Setup imports from future files

---
+++ makefile
@@ -0,0 +1,52 @@
+src_files	:= $(wildcard ./src/*.asm)
+src_names	:= $(patsubst ./src/%.asm, %, $(src_files))
+targ_objs	:= $(patsubst %, ./target/%.o, $(src_names))
+std			:= $(patsubst %, ../std/target/%, $(shell ls ../std/target | grep "\.o" | grep -v "main\.o"))
+libs		:= $(patsubst %, ../%/target/%.o, $(shell cat ./libs))
+
+w	:= $(shell tput cols)
+h	:= $(shell tput lines)
+
+.SILENT:
+
+build: compile link
+run: clean build exec
+gdb: clean build startgdb
+
+compile: $(src_files)
+	echo "+ Building files:"
+	for f in $(src_names); do \
+		echo "  - $$f.asm"; \
+		nasm -f elf -o ./target/$$f.o ./src/$$f.asm; \
+	done
+
+link: $(targ_objs) $(libs) $(std)
+	echo "+ Linking files:"
+	for f in $^; do echo "  - $$f"; done
+	ld -m elf_i386 -o ./target/main $^
+
+exec: ./target/main
+	echo "+ Executing main"
+	echo "  With ($(w), $(h))"
+	echo "=================="
+	./target/main $(w) $(h)
+
+clean:
+	echo "+ Cleaning Target"
+	rm -rf ./target
+	mkdir target
+
+init:
+	read -p "Enter new project name: " proj_name && \
+	mkdir -p $$proj_name/src && \
+	cp template.asm $$proj_name/src/main.asm && \
+	cd $$proj_name && \
+	touch libs && \
+	ln ../makefile makefile
+
+addlib:
+	read -p "Enter lib name: " lib_name && \
+	echo $$lib_name >> ./libs
+
+startgdb:
+	gdb --args ./target/main $(w) $(h)
---
+++ src/main.asm
@@ -0,0 +1,95 @@
+%define syscall		int 80h		; call the kernel
+
+%macro stringln 2		; Macro to make string declarations easier
+	%1: db %2, 0xA
+	%1_len: equ $-%1
+%endmacro
+%macro string 2		; Macro to make string declarations easier
+	%1: db %2
+	%1_len: equ $-%1
+%endmacro
+%macro buffer 2
+	%1: times %2 db 0
+	%1_len: equ $-%1
+%endmacro
+
+%macro sphere 9
+	%1_position:	dd %2, %3, %4
+	%1_radius:		dd %5
+	%1_ambient:		dd %6
+	%1_diffuse:		dd %7
+	%1_specular:	dd %8
+	%1_shininess:	dd %9
+%endmacro
+
+section .data
+	stringln helloWorld, "Hello, world!"
+	width: dd 75
+	height: dd 70
+	v1: dd 1.0, 2.0, 3.0
+	v2: dd 3.0, 2.0, 1.0
+	rad: dd 0.7,
+
+section .text
+	global _start
+	extern _parse_num, _print_num, _put_char			; std
+	extern _dot, _norm, _normalise, _vec_add, _vec_sub	; vectors
+	extern _sphere_intersection							; spheres
+
+_start:
+	jmp _end
+	pop ecx ; ETX
+	pop ecx ; path
+	pop ecx ; width or null
+	cmp ecx, 0x0
+	je _no_args
+	mov edx, 4
+	call _parse_num
+
+	xor edx, edx
+	mov ebx, 0x2
+	div ebx
+	mov [width], eax
+
+	pop ecx ; height or null
+	cmp ecx, 0x0
+	je _no_args
+	mov edx, 4
+	call _parse_num
+
+	mov ebx, 0xA
+	sub eax, ebx
+	mov [height], eax
+_no_args:
+	mov eax, [width]
+	mov ebx, 0xA
+	call _print_num
+	mov eax, 0xA
+	call _put_char
+	mov eax, [height]
+	mov ebx, 0xA
+	call _print_num
+	mov eax, 0xA
+	call _put_char
+
+	mov eax, 4				; Put the system call for write
+	mov ebx, 1				; To the std output
+	mov ecx, helloWorld		; Give the *char
+	mov edx, helloWorld_len	; Give the length of the buffer
+	syscall					; Tell the kernel to do it
+
+_end:
+; stack:
+;(+0x00) *ret_location
+; +0x04 *center
+; +0x08 *radius
+; +0x0B *ray_origin
+; +0x10 *ray_direction
+	push v1
+	push v2
+	push rad
+	push v1
+	call _sphere_intersection
+	mov eax, 1		; Exit
+	mov ebx, 0		; With no error
+	syscall
